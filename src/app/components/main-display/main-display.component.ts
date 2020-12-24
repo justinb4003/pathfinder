e5d845a8d259cdf869329fc4e50df1c697e765c8
Justin Buist <justin.buist@gmail.com>
2020-12-24 15:46:38 -0500

    Added three

--- src/app/components/main-display/main-display.component.ts
+++ src/app/components/main-display/main-display.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-main-display',
